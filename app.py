da2781057a17107f20fd1c512f9de22e5a9e7630
Kat Koler <katjusa.koler@gmail.com>
2019-03-20 15:30:43 +0000

    try without dotenv on heroku

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request
 import json
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # load dotenv in the base root
-APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
-dotenv_path = os.path.join(APP_ROOT, '.env')
-load_dotenv(dotenv_path)
+# APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
+# dotenv_path = os.path.join(APP_ROOT, '.env')
+# load_dotenv(dotenv_path)
 
 
 consumer_key = os.getenv("twitter_consumer_key")
